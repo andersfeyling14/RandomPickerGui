60ea52e9291dedee70abb37f26a4861c449ac0fc
Anders Feyling <feylinganders@gmail.com>
2018-11-08 22:55:01 +0100

    add initial files

---
+++ RandomPickerGui/RandomPickerGui/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomPickerGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string line;
+            string[] nameArray;
+
+            try
+            {
+                StreamReader sr = new StreamReader(@"../../names.txt");
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+                sr.Close();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("The file could not be read");
+                Console.WriteLine(error.Message);
+            }
+            Console.ReadKey();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
